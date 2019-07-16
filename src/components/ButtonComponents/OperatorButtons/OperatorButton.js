5200abd75e2a04e0c89c8c8163f7561240b741f1
gisellehargrove <gisellehargrove@gmail.com>
2019-07-16 16:03:24 -0400

    display operator components

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,6 +4,9 @@ const OperatorButton = (props) => {
   return (
     <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props.text}</button>
     </div>
   );
 };
+
+export default OperatorButton;
